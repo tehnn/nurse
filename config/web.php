d7a4c74026f105b504b63554d519582ec0ba5a22
DHDC Main <utehnphnu2015@gmail.com>
2015-05-13 16:13:56 +0700

    tehnn,20150504

--- config/web.php
+++ config/web.php
@@ -6,7 +6,8 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'components' => [
+    'language'=>'th',
+    'components' => [        
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '5dBy-Ymt2Zo3T5JXJK1bZHHgrQenC6nF',
--- views/kpi-sub-hos/view.php
+++ views/kpi-sub-hos/view.php
@@ -8,12 +8,11 @@ use yii\widgets\DetailView;
 
 $this->title = $model->year;
 $this->params['breadcrumbs'][] = ['label' => 'Kpi Sub Hos', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="kpi-sub-hos-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
+  
     <p>
         <?= Html::a('Update', ['update', 'year' => $model->year, 'month' => $model->month, 'hospcode' => $model->hospcode], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'year' => $model->year, 'month' => $model->month, 'hospcode' => $model->hospcode], [
